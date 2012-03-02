e94bc179568dc98e0d08074d5908c0a48432d1fb
Nicolas Burrus <nicolas.burrus@manctl.com>
2012-03-02 14:06:35 +0100

    Fix include path.

--- openni-config.cmake
+++ openni-config.cmake
@@ -1,4 +1,4 @@
 GET_FILENAME_COMPONENT(HERE "${CMAKE_CURRENT_LIST_FILE}" PATH)
 GET_FILENAME_COMPONENT(INCLUDE_DIR ${HERE}/../../include ABSOLUTE)
-include_directories(${INCLUDE_DIR})
+include_directories(${INCLUDE_DIR}/ni)
 include(${HERE}/openni-exports.cmake)
